9c055edf003add55a224063c6fd9cfac0576ff1a
qiqioberon <aquq1q1.farrukh@gmail.com>
2024-09-08 23:47:26 +0700

    feat: add slicing kalkulator page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Pertemuan1\FormController;
+
+Route::get('/form', [FormController::class, 'showForm'])->name('form.show');
+Route::post('/form', [FormController::class, 'handleForm'])->name('form.handle');
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,4 +13,14 @@ Route::get('/', function () {
 
 Route::get('/form_get', function () {
     return view('FormGet');
+});
+
+
+Route::get('/form_post', function () {
+    return view('FormPost');
+});
+
+
+Route::get('/kalkulator', function () {
+    return view('Pertemuan2.Kalkulator.kalkulator');
 });
