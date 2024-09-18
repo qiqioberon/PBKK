cebd1f417beeb3ee5a65a31452637c56ccdf32eb
qiqioberon <aquq1q1.farrukh@gmail.com>
2024-09-19 03:22:06 +0700

    feat: integrate input output image

--- resources/views/Pertemuan3/ImageProcessing/imageoutput.blade.php
+++ resources/views/Pertemuan3/ImageProcessing/imageoutput.blade.php
@@ -17,14 +17,14 @@
             <div class="w-full flex flex-col justify-center items-center gap-10 px-24">
                 <div class="flex flex-row gap-3 py-5 items-start w-full">
                     <div class="flex justify-center items-center gap-3">
-                        <button class="hover:bg-[#6f6f71] rounded-full p-3">
+                        <a href={{ route('input.image') }} class="hover:bg-[#6f6f71] rounded-full p-3">
                             <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48"
                                 fill="none">
                                 <path fill-rule="evenodd" clip-rule="evenodd"
                                     d="M23.2971 40.0971C22.3599 41.0343 20.8403 41.0343 19.903 40.0971L5.50299 25.6971C4.56573 24.7598 4.56573 23.2402 5.50299 22.3029L19.903 7.90295C20.8403 6.96569 22.3599 6.96569 23.2971 7.90295C24.2344 8.8402 24.2344 10.3598 23.2971 11.2971L12.9942 21.6H40.8001C42.1255 21.6 43.2001 22.6745 43.2001 24C43.2001 25.3255 42.1255 26.4 40.8001 26.4L12.9942 26.4L23.2971 36.7029C24.2344 37.6402 24.2344 39.1598 23.2971 40.0971Z"
                                     fill="#F9F9FA" />
                             </svg>
-                        </button>
+                        </a>
                         <p class="text-white">KEMBALI KE UPLOAD FOTO</p>
                     </div>
                 </div>
@@ -46,12 +46,12 @@
                     </div>
                     <div
                         class="image flex w-full h-[500px] p-4 justify-center items-center self-stretch border border-[#E5E5E6] bg-[#F9F9FA]">
-                        <img src="{{ Vite::asset('resources/images/ImageProcess/sampleImage.png') }}" alt="imageoutput"
+                        <img src="{{ asset('images/' . $imageName) }}" alt="imageoutput"
                             class="w-full h-full object-cover" />
                     </div>
                     <div
                         class="download flex w-full items-start p-4 bg-[#E5E5E6] border border-[#E5E5E6] rounded-xl justify-center ">
-                        <button
+                        <a href="{{ asset('images/' . $imageName) }}" download="{{ $imageName }}"
                             class="text-[#212122] bg-[#54C4DB] py-2 px-5 flex flex-row gap-3 justify-center items-center rounded-xl border-whites border-2 w-[50%] hover:bg-[#ffe9e9]">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none">
@@ -60,7 +60,7 @@
                                     stroke="#212122" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                             <p class="text-base font-medium">Download</p>
-                        </button>
+                        </a>
                     </div>
                 </div>
 
@@ -76,12 +76,12 @@
                     </div>
                     <div
                         class="image flex w-full h-[500px] p-4 justify-center items-center self-stretch border border-[#E5E5E6] bg-[#F9F9FA]">
-                        <img src="{{ Vite::asset('resources/images/ImageProcess/sampleImage.png') }}" alt="imageoutput"
+                        <img src="{{ asset('images/' . $imageName) }}" alt="imageoutput"
                             class="w-full h-full object-cover" />
                     </div>
                     <div
                         class="download flex w-full items-start p-4 bg-[#E5E5E6] border border-[#E5E5E6] rounded-xl justify-center ">
-                        <button
+                        <a href="{{ asset('images/' . $imageName) }}" download="{{ $imageName }}"
                             class="text-[#212122] bg-[#54C4DB] py-2 px-5 flex flex-row gap-3 justify-center items-center rounded-xl border-whites border-2 w-[50%] hover:bg-[#ffe9e9]">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none">
@@ -90,7 +90,7 @@
                                     stroke="#212122" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                             <p class="text-base font-medium">Download</p>
-                        </button>
+                        </a>
                     </div>
                 </div>
                 <div class="card flex flex-col w-[45%] items-center justify-center gap-4">
@@ -105,12 +105,12 @@
                     </div>
                     <div
                         class="image flex w-full h-[500px] p-4 justify-center items-center self-stretch border border-[#E5E5E6] bg-[#F9F9FA]">
-                        <img src="{{ Vite::asset('resources/images/ImageProcess/sampleImage.png') }}" alt="imageoutput"
+                        <img src="{{ asset('images/' . $imageName) }}" alt="imageoutput"
                             class="w-full h-full object-cover" />
                     </div>
                     <div
                         class="download flex w-full items-start p-4 bg-[#E5E5E6] border border-[#E5E5E6] rounded-xl justify-center ">
-                        <button
+                        <a href="{{ asset('images/' . $imageName) }}" download="{{ $imageName }}"
                             class="text-[#212122] bg-[#54C4DB] py-2 px-5 flex flex-row gap-3 justify-center items-center rounded-xl border-whites border-2 w-[50%] hover:bg-[#ffe9e9]">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                 viewBox="0 0 24 24" fill="none">
@@ -120,7 +120,7 @@
                                     stroke-linejoin="round" />
                             </svg>
                             <p class="text-base font-medium">Download</p>
-                        </button>
+                        </a>
                     </div>
                 </div>
 
--- routes/pertemuan3.php
+++ routes/pertemuan3.php
@@ -2,12 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/imageinput', function () {
-    return view('Pertemuan3.ImageProcessing.imageinput');
-});
+use App\Http\Controllers\Pertemuan3\ImageUploadController;
 
 
-Route::get('/imageoutput', function () {
-    return view('Pertemuan3.ImageProcessing.imageoutput');
-});
+Route::get('/imageinput', [ImageUploadController::class, 'showForm'])->name('input.form');
+Route::post('/imageinput', [ImageUploadController::class, 'upload'])->name('input.image');
+Route::get('/imageoutput', [ImageUploadController::class, 'showImage'])->name('output.image');
